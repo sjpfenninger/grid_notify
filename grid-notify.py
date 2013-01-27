5e5793eeaa6464a7e3b1d77d63a28d10ec7d5049
Stefan Pfenninger <stefan@pfenninger.org>
2013-01-27 10:03:00 +0000

    Also display job's name; Some docstrings added

--- grid-notify.py
+++ grid-notify.py
@@ -11,6 +11,7 @@ import pushnotify  # version >= 0.5
 
 
 def read_configuration():
+    """Read the configuration file and return a dict containing the settings"""
     script_path = os.path.dirname(os.path.realpath(__file__))
     config = ConfigParser.RawConfigParser()
     config.read(os.path.join(script_path, 'grid-notify.conf'))
@@ -25,13 +26,16 @@ def read_configuration():
 
 
 def parse_return(string):
+    """Parse the return string from qsub and get the task_id and name, returning them as a tuple"""
     assert string[0:4] == 'your'  # Because we have no clue what's going on if we're not getting 'your'
     identifier = string.split()[2]  # This could still be in the form 123.1-10:1 if it's a job-array
     task_id = identifier.split('.')[0]  # Gives us the task_id whether identifier was 123 or 123.1-10:1
-    return int(task_id)  # Implicitly ensure that we're returning an int, or an exception will be raised
+    name = string.split()[3][2:-2]
+    return (int(task_id), name)  # Implicitly ensure that we're returning an int for task_id, or an exception will be raised
 
 
 def make_path_absolute(path):
+    """Helper function to rewrite a given executable path into an absolute path if necessary to find the executable"""
     if os.path.isabs(path):
         return path
     else:
@@ -44,21 +48,29 @@ def make_path_absolute(path):
 
 
 def run_and_get_task(script, print_output=True):
+    """Run a Grid Engine submission script via subprocess, and return a tuple of the resulting task_id and name
+
+    Args:
+        print_output : if True (default), print the output from the submission script to standard out, otherwise, be silent.
+
+    """
     p = subprocess.Popen(script.split(' '), stdout=subprocess.PIPE)
     string = p.stdout.readlines()[0]  # If all went well we get a single line with the qsub result
     if print_output:
         print string
-    task_id = parse_return(string)
-    return task_id
+    task_id, name = parse_return(string)
+    return (task_id, name)
 
 
 def setup_notifier(api, api_key, title):
+    """Set up a notifier with the given api, api_key and title; return a notifier instance"""
     push_client = pushnotify.get_client(api, application=title)
     push_client.add_key(api_key)
     return push_client
 
 
 def _pretty_time_difference(start, end):
+    """Return a string with nicely formatted time difference between the two UNIX time staps `start` and `end`"""
     elapsed = end - start
     secs = datetime.timedelta(seconds=elapsed)
     d = datetime.datetime(1, 1, 1) + secs
@@ -73,17 +85,34 @@ def _pretty_time_difference(start, end):
     return elapsed_string + ' ' + elapsed_format
 
 
-def notify(notifier, task_id, start=None):
-    """If start given, assume it is a UNIX timestamp and also print time difference between start and current time"""
+def notify(notifier, task_id, start=None, name=None):
+    """ Send a notification via `notifier`
+
+    Args:
+        task_id : task_id of the completed task, used in the event description
+        start : if given, assume it is a UNIX timestamp and also print time difference between start and current time
+        name : name of task, if given, used in the event's name
+
+    """
     current_time = time.strftime("%Y-%m-%d %H:%M")
     descr = 'Task {} done @ {}.'.format(task_id, current_time)
     if start:
         elapsed = _pretty_time_difference(start, time.time())
         descr += ' Duration: {}.'.format(elapsed)
-    notifier.notify(description=descr, event='Tasks complete.')
+    if name:
+        event_name = '{} complete.'.format(name)
+    else:
+        event_name = 'Tasks complete.'
+    notifier.notify(description=descr, event=event_name)
 
 
 def monitor(task_id, user=None):
+    """Check whether a given task_id is completed every 30 seconds, and return True once it's completed
+
+    Args:
+        user : if given, only list the user's tasks when getting running tasks, which probably reduces the load on the machine..
+
+    """
     if user is None:
         command = ['qstat']
     else:
@@ -108,6 +137,7 @@ def _force_fork():
 
 
 def daemonify():
+    """Helper function to push the script into the background, returning control to the terminal"""
     # Source: http://motoma.io/daemonizing-a-python-script/
     _force_fork()
     #os.chdir("/")
@@ -128,11 +158,14 @@ if __name__ == '__main__':
     config = read_configuration()
     parser = argparse.ArgumentParser(description=os.path.basename(__file__))
     parser.add_argument('script', metavar='script', type=str, help='Grid Engine submission script to monitor.')
+    parser.add_argument('-n', '--name', dest='name', type=str, default=None, help='Friendly name to use when sending notification (if not given, uses the Grid Engine job\'s name).')
     parser.add_argument('-p', '--process', dest='process', action='store_const', const=True, default=False, help='Do post-processing by calling process_{script}.')
     args = parser.parse_args()
     script_path = make_path_absolute(args.script)
     start_time = time.time()
-    task_id = run_and_get_task(script_path)
+    task_id, name = run_and_get_task(script_path)
+    if args.name:
+        name = args.name
     daemonify()  # Push the script into the background so control is returned to terminal
     notifier = setup_notifier(api=config['api'], api_key=config['api_key'], title=config['title'])
     user = os.environ['LOGNAME']
@@ -140,5 +173,5 @@ if __name__ == '__main__':
     if args.process:
         postprocess(script_path)
         task_id = str(task_id) + ' & post-processing'
-    notify(notifier, task_id, start=start_time)
+    notify(notifier, task_id=task_id, start=start_time, name=name)
     subprocess.call(["echo", "-e", "\a"])  # Terminal bell
